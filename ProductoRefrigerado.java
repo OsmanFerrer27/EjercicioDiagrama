5525fdea62447b2b63a9820d339e483a63764923
OsmanFerrer27 <91578666+OsmanFerrer27@users.noreply.github.com>
2021-10-20 14:45:42 -0400

    Update ProductoRefrigerado.java

--- ProductoRefrigerado.java
+++ ProductoRefrigerado.java
@@ -4,8 +4,9 @@ public class ProductoRefrigerado extends ProductoFrio {
 
 	private String codSupervision;
 	
-	public ProductoRefrigerado(String fechaCaducidad, int numeroLote, String fechaEnvasado, String paisDeOrigen) {
+	public ProductoRefrigerado(String fechaCaducidad, int numeroLote, String fechaEnvasado, String paisDeOrigen, String codSupervision) {
 		super(fechaCaducidad, numeroLote, fechaEnvasado, paisDeOrigen);
+		this.codSupervision = codSupervision;
 	}
 
 	public String getCodSupervision() {
